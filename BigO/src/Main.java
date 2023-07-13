fdec0332c8f9054462544f5bdf77d30df64795e8
Gowri Shankar <gowrishankarmukkeri@gmail.com>
2023-07-13 12:45:30 +0530

    O(1) code

---
+++ BigO/src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+         int[] numbers = {1,2,3};
+         print(numbers);
+         //if the input has million elements also we required 1 operation to get number using index
+        //thats why O(1)
+    }
+    public static void print(int[] numbers){
+        System.out.println(numbers[0]);
+    }
+}
